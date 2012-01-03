9af4c17d9dd8ed25cbd6a23e61ce4c8aca35024c
Steph Skardal <steph@endpoint.com>
2012-01-02 22:03:19 -0500

    Bug fix.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,6 +1,10 @@
 class ImageController < ApplicationController
   def show
     @image = Image.find_by_slug(params[:id])
-    @related_products = @image.categories.first.images.select { |p| p != @image }[0..2]
+    if @image.categories.any?
+      @related_products = @image.categories.first.images.select { |p| p != @image }[0..2]
+    else
+      @related_products = []
+    end
   end
 end
