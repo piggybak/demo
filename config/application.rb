bf9f994e922956b05bb60383c7f4d58e8bf0882b
Piggy Bak <piggybak@v314.spreecamps.com>
2012-01-10 11:21:22 -0500

    Remove migration. Update to production.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ end
 
 module Demo
   class Application < Rails::Application
-    Rails.env = ActiveSupport::StringInquirer.new('development')
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
