7b422cf92dd3c9a694b4ab3b390345e4d53659bb
Steph Skardal <steph@endpoint.com>
2012-01-10 21:00:17 -0500

    Emphasis on demo link. Sample data fix.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module Demo
   class Application < Rails::Application
+    Rails.env = ActiveSupport::StringInquirer.new('development')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
