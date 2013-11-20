01fe32dd174b9d826fa944f77c987f3b41a4614a
Steph Skardal <steph@endpoint.com>
2013-11-20 12:13:52 -0500

    WIP Upgrade.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.12'
+gem 'rails', '3.2.15'
 gem 'fastercsv'
 gem 'rails_admin'
 gem 'pg', '0.12.0'
--- config/routes.rb
+++ config/routes.rb
@@ -69,4 +69,7 @@ Demo::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  # Catchall e.g.
+  # match '*path' => 'home#index' 
 end
